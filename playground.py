ce934c24d08bdb21a610ad5879e1d8d94d4b92e4
Willi Raschkowski <willi.raschkowski@student.hpi.de>
2015-12-09 03:40:30 +0100

    Implemented k-means

--- playground.py
+++ playground.py
@@ -1,29 +1,59 @@
-from math import log
+import numpy as np
 
-def entropy(data):
-    labels = [d[1] for d in data] # Get label for each data point
-    distinct_labels = set(labels) # Get distinct labels in set
+points = np.array([[3, 8], [4, 7], [3, 6], [3, 4], [4, 5],
+                   [5, 5], [5, 2], [8, 4], [9, 4], [9, 1]])
+labels = np.array([0, 0, 0, 0, 1, 1, 1, 1, 1, 1])
+
+
+def iterate_k_means(points, labels):
+    unique_labels = np.unique(labels)
+    means = np.zeros(shape=(len(unique_labels), 2))
+    for label in unique_labels:
+        # Get mask to select points that have this label
+        mask = (labels == label)
+        # Calculate the center of all those points
+        # (Manhattan distance) using the average
+        means[label] = np.average(points[mask], axis=0)
+
+    distances = np.ndarray(shape=(len(unique_labels), len(points)))
+    for i, mean in enumerate(means):
+        # For each mean calculate an array with its distance to each point
+        distances[i] = abs(points - mean).sum(axis=1)
+
+    # Get the minimum distance for each point
+    min_distances = distances.min(axis=0)
+
+    # Get an array of masks (for each) label by
+    # checking where the min_distance of each point is
+    masks = (distances == min_distances)
+
+    # Generate new labels by writing the label number
+    # where the mask says 'True'
+    labels = np.ndarray(shape=(len(points), 1))
+    for i, mask in enumerate(masks):
+        labels[mask] = i
+
+    return labels
+
+
+def log(points, labels):
+    unique_labels = np.unique(labels)
+    means = np.zeros(shape=(len(unique_labels), 2))
+    for label in unique_labels:
+        # Get mask to select points that have this label
+        mask = (labels == label)
+        # Calculate the center of all those points
+        # (Manhattan distance) using the average
+        means[label] = np.average(points[mask], axis=0)
     
-     # If there is only one label return entropy of 1 ( to avoid log_2(1) )
-    if len(distinct_labels) == 1: 
-        return 1.0
+    for i, mean in enumerate(means):
+        print("Mean %d: %s" % (i, mean))
 
-    probs = [ # Calculate probabilites for each distinct label
-        labels.count(label)/len(data) # Count of each label by total number of occurrences
-        for label in distinct_labels]
+    print("\nPoint\t\t\tLabel")
+    for i, p in enumerate(points):
+        print("(%.2f, %.2f):\t%d" % (p[0], p[1], labels[i]))
 
-    # Calculate total entropy by weighting the number of bits necessary to
-    # represent the probability ( log_2(probability) ) by the probability itself
-    return -sum([p * log(p, 2) for p in probs]) 
 
-data = [(0.1,   'FAIL'),
-        (0.2,   'FAIL'),
-        (0.8,   'FAIL'),
-        (0.9,   'OK'),
-        (1.0,   'FAIL'),
-        (4.0,   'OK'),
-        (10.0,  'OK'),
-        (50.0,  'OK')]
 
-print(entropy(data))
-
+# iterate_k_means(points, labels)
+log(points, labels)
